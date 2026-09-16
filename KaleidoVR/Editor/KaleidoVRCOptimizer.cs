99193798b268db166ccf838079a6007d42594df3
KaleidoVR <KaleidoVR@hotmail.com>
2026-09-15 20:22:44 -0700

    Mark the build as 1.0.64.

--- KaleidoVR/Editor/KaleidoVRCOptimizer.cs
+++ KaleidoVR/Editor/KaleidoVRCOptimizer.cs
@@ -194,7 +194,7 @@ namespace KaleidoVR.EditorTools
 
     public class KaleidoVRCOptimizer : EditorWindow
     {
-        public static readonly string VERSION = "1.0.63";
+        public static readonly string VERSION = "1.0.64";
         public const float WindowMinWidth = 660f;
         public const float WindowMinHeight = 720f;
         public const string LOGO_FILE_NAME = "Kali_Logo.png";
